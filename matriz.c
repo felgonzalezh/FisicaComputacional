3cffd52de4b1338f936cac02f5ce1dc1e63dc0aa
Carlos Gonzalez <felgonzalezh@gmail.com>
2013-02-26 14:32:24 -0500

    tarea3 leyendo array

--- matriz.c
+++ matriz.c
@@ -2,8 +2,10 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_eigen.h>
 
-
+/*declaracion de funciones*/
 float *load_data(char *filein, int *nf, int *nc);
+
+/*funcion main*/
 int main (int arcg, char **argv){
   
   FILE *in;
@@ -21,12 +23,16 @@ int main (int arcg, char **argv){
   data=load_data(filein, &nf, &nc);
   
   return 0;
-}
+  }
+
+
+/*funciones*/
 float *load_data(char *filein, int *nf, int *nc){
   
   FILE *in;
   char c;
   float *array;
+  double t,x;
   int l, i;
   i=0;
   
@@ -49,13 +55,13 @@ float *load_data(char *filein, int *nf, int *nc){
   rewind(in);
   
   printf("Now I am reading\n");
-  /*  
-       for(i=0;i<l;i++){
-	 fscanf(in, "%d \n", nf);
-	 printf("value = %d \n", nf);
-	 }
-	 fclose(in);*/
+    
+  for(i=0;i<l;i++){
+    fscanf(in, "%f %f\n", &(array[2*i]), &(array[2*i +1]));
+      printf("%f %f\n", array[i*2], array[2*i +1]);
+  }
   
+  fclose(in);  
   return array;
 }
 
